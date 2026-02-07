643d890083b028ba511b82f832e02b2adb670534
Abhinav-Ayyala <layyalas@gitam.in>
2026-02-07 21:24:13 +0530

    Day 2: practiced data types

---
+++ day02_datatypes.py
@@ -0,0 +1,11 @@
+# Day 2 - Data Types
+
+integer_value = 25
+float_value = 3.5
+string_value = "Python"
+boolean_value = True
+
+print(integer_value, type(integer_value))
+print(float_value, type(float_value))
+print(string_value, type(string_value))
+print(boolean_value, type(boolean_value))
